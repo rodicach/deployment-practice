363c610d331c44e8a3e62afb686eaaded01253d6
rodicach <rodicachavez@pursuit.org>
2022-10-16 15:53:15 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const[drinks,setDrinks] = useState([]);
+
+  function getDrinksOnClick () {
+    fetch ('https://the-cocktail-db.p.rapidapi.com/popular.php', {
+      method: 'GET',  
+      headers: {
+        'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+        'X-RapidAPI-Host': 'the-cocktail-db.p.rapidapi.com'
+      }
+    }).then(res => res.json()).then(data => {
+      setDrinks(data.drinks);
+    })
+  }
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={getDrinksOnClick}>Get Drinks</button>
+      {
+        drinks.map(d => {
+          return(
+            <div>
+              { d.strDrink }
+            </div>
+          )
+        })
+      }
     </div>
   );
 }
